4f0349ab6b8951557c4040f6d786192a9819e5c1
Helio Medeiros <heliocmedeiros@gmail.com>
2025-06-16 21:41:53 +0200

    feat(game): add labyrinth with stone walls and comprehensive tests
    Create complex labyrinth structure with stone walls, dead ends, and multiple paths. Move VIM keys into labyrinth to encourage exploration and learning. Fix TileType static field syntax for ESLint compatibility.

--- src/domain/value-objects/TileType.js
+++ src/domain/value-objects/TileType.js
@@ -1,27 +1,29 @@
 export class TileType {
-    static GRASS = new TileType('grass', true);
-    static WATER = new TileType('water', false);
-    static DIRT = new TileType('dirt', true);
-    static TREE = new TileType('tree', false);
+  constructor(name, walkable) {
+    this._name = name;
+    this._walkable = walkable;
+  }
 
-    constructor(name, walkable) {
-        this._name = name;
-        this._walkable = walkable;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get walkable() {
+    return this._walkable;
+  }
 
-    get walkable() {
-        return this._walkable;
-    }
+  equals(other) {
+    return other instanceof TileType && this._name === other._name;
+  }
 
-    equals(other) {
-        return other instanceof TileType && this._name === other._name;
-    }
-
-    toString() {
-        return this._name;
-    }
+  toString() {
+    return this._name;
+  }
 }
+
+// Define static instances after class definition
+TileType.GRASS = new TileType('grass', true);
+TileType.WATER = new TileType('water', false);
+TileType.DIRT = new TileType('dirt', true);
+TileType.TREE = new TileType('tree', false);
+TileType.STONE = new TileType('stone', false);
